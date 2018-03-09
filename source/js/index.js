775dcf598a589742295444a874f76a96a555b0e7
unknown <afrazafaq96@gmail.com>
2018-03-10 01:34:03 +0500

    Solved issue no 3 SideNav left to right

--- source/js/index.js
+++ source/js/index.js
@@ -4,7 +4,13 @@ let materialize = require('../../node_modules/materialize-css/dist/js/materializ
 
 
 $(document).ready(function () {
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true, // Choose whether you can drag to open on touch screens,
+  }
+);
   $('.parallax').parallax();
   $('.carousel').carousel({ indicators: true, padding: 50, shift: 80 });
   let setCopyRightYear = () => {
